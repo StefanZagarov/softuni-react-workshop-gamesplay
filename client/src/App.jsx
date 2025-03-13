c20504fa56560765a4ea809b2600260051a3f797
StefanZagarov <stefanzagarov@gmail.com>
2025-03-13 17:14:36 +0200

    Added routes and pages to all navigation links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
+import { Routes, Route } from 'react-router';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
-import { Routes, Route } from 'react-router';
+import Login from './components/login/Login';
+import Register from './components/register/Register';
+import GamesCatalog from './components/games-catalog/GamesCatalog';
+import GameDetails from './components/game-details/GameDetails';
+import GameCreate from './components/game-create/GameCreate';
 
 function App() {
 
@@ -14,6 +19,11 @@ function App() {
       <main id="main-content">
         <Routes>
           <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="games" element={<GamesCatalog />} />
+          <Route path="games/create" element={<GameCreate />} />
+          <Route path="games/:id" element={<GameDetails />} />
         </Routes>
       </main>
 
